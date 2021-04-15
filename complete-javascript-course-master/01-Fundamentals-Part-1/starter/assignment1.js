b1bed5e06255cf8c113eaa1d00b5c43c49f90e18
vignesh <vignesh787@gmail.com>
2021-04-15 17:49:26 +0530

    Initial Commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment1.js
@@ -0,0 +1,7 @@
+let country = "INDIA";
+let continent = "ASIA";
+let population = 1366;
+
+console.log({country});
+console.log({continent});
+console.log({population});
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+
+let js = 'amazing';
+
+console.log(1987 + 07 + 04);
+
+var firstName = "Vignesh";
+console.log({firstName});
+
+let vignesh_ramesh  = "RV";
+console.log(vignesh_ramesh);
+
+let PI=3.1415;
+console.log({PI});
