5e1b55a4423c031bd09403280ba1cd1bf9486090
vignesh <vignesh787@gmail.com>
2021-04-21 07:57:16 +0530

    JS Module 2 Development

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,147 @@
+const age  = 19;
+
+const isOldEnough = age>=18;
+if(isOldEnough)
+{
+    console.log(` Age is greater than 18 - ${age}`);
+}
+
+
+
+
+
+
+
+
+
+
+// JS String Template
+/*
+const firstName = 'Vignesh';
+const job = 'Architect';
+const birthYear = 1987;
+const year = 2037;
+
+const vignesh ="I'm " + firstName + ', a ' + (year - birthYear) +' years old ' + job + '!';
+
+console.log(vignesh);
+
+// Template literals to solve above problem
+
+const vigneshnew = `I'm ${firstName} , a ${year - birthYear} years old ${job}`;
+
+console.log(vigneshnew);
+
+console.log(' String with \n multiple \n\ lines');
+
+console.log(`String 
+multiple 
+lines`);
+*/
+
+
+// BMI = mass/ height * height
+/*
+const markHeight = 1.69;
+const johnHeight = 1.95;
+const markMass = 78;
+const johnMass = 92;
+*/
+/*const markHeight = 1.88;
+const johnHeight = 1.76;
+const markMass = 95;
+const johnMass = 85;
+
+
+
+let markBMI,johnBMI,higherBMI;
+
+markBMI = markMass/(markHeight ** 2);
+johnBMI = johnMass/(johnHeight * johnHeight);
+
+console.log({markHeight,markMass,markBMI});
+
+console.log({johnHeight,johnMass,johnBMI});
+
+higherBMI = markBMI > johnBMI;
+
+console.log(higherBMI);
+*/
+
+/*
+// Operator Precedence 
+const now = 2037;
+
+const ageVignesh = now-1987;
+const ageVedhaanth = now-2015;
+
+console.log(now-1987 > now - 2015);
+
+console.log(25-10-5);
+
+let x, y;
+x = y = 25-10-5;
+
+console.log({x,y});
+
+console.log({ageVignesh,ageVedhaanth});
+
+ let avgAge = (ageVignesh+ageVedhaanth)/2;
+ console.log(avgAge);
+ avgAge = ageVignesh+ageVedhaanth/2;
+ console.log(avgAge);
+
+
+
+
+
+//Operators
+
+const now = 2037;
+
+const ageVignesh = now-1987;
+const ageVedhaanth = now-2015;
+console.log({ageVignesh,ageVedhaanth});
+console.log({ageVedhaanth});
+console.log(ageVedhaanth*2 , ageVignesh/2 , 3**7);
+
+console.log('Vignesh',34 , 3**4);
+
+const firstName = "Vignesh";
+const lastName = "Ramesh";
+console.log(firstName+ ' '+ lastName);
+
+let x = 10 + 5;
+console.log(x);
+x+=10
+console.log(x);
+x*=4;
+x++;
+x--;
+console.log(x);
+
+console.log(ageVignesh > ageVedhaanth);
+console.log(ageVignesh < ageVedhaanth);
+console.log(ageVignesh === ageVedhaanth+28);
+
+
+let age = 30; // when it can change later
+
+age = 31;
+
+const birthYear = 1987;
+
+console.log({birthYear});
+console.log(typeof(birthYear));
+
+var job ='programmer';
+
+job = 'architect';
+
+console.log({job});
+
+lastName = "lastname";
+console.log({lastName});
 
 let js = 'amazing';
 
@@ -10,4 +154,26 @@ let vignesh_ramesh  = "RV";
 console.log(vignesh_ramesh);
 
 let PI=3.1415;
-console.log({PI});
+console.log({PI});*/
+
+/*
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof(javaScriptIsFun));
+console.log(typeof(false));
+console.log(typeof(23));
+console.log(typeof("javaScriptIsFun"));
+
+javaScriptIsFun = 'YES!!';
+
+console.log(typeof(javaScriptIsFun));
+console.log(javaScriptIsFun);
+
+let year;
+year = 1991;
+console.log(year);
+console.log(typeof(year));
+
+console.log(typeof null);
+*/
