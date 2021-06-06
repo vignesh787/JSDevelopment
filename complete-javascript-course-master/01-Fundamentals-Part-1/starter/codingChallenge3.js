245f3db744eac557c2f30c72f9972c0db78a17c5
vignesh <vignesh787@gmail.com>
2021-06-06 11:56:30 +0530

    windows git update

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/codingChallenge3.js
@@ -0,0 +1,28 @@
+console.log("Windows Git Update");
+// Dolphins - 96 , 108 , 89
+// Koalas  - 88 , 91 , 110
+
+// const avgDolphins = (96 + 108 + 89) / 3;
+// const avgKoalas = (88 + 100 + 110)/ 3;
+// console.log(avgDolphins,avgKoalas);
+// if(avgDolphins > avgKoalas){
+//     console.log("Dolphins have higher average than Koalas");
+// } else if(avgKoalas > avgDolphins){
+//     console.log("Koalas have higher average than Dolphins");
+// } else{
+//     console.log("The average of Dolphins and Koalas are Same !!")
+//}
+
+//Bonus 1
+const avgDolphins = (97 + 112 + 101) / 3;
+const avgKoalas = (109 + 95 + 103)/ 3;
+console.log(avgDolphins,avgKoalas);
+if(avgDolphins > avgKoalas && avgDolphins >= 100) {
+    console.log("Dolphins have higher average than Koalas");
+} else if(avgKoalas > avgDolphins && avgKoalas >= 100 ){
+    console.log("Koalas have higher average than Dolphins");
+} else{
+    console.log("The average of Dolphins and Koalas are Same !!")
+}
+
+
