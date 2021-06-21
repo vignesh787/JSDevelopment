048f8ea60197e1436bf4b40b59c03127c13df840
vignesh <vignesh787@gmail.com>
2021-06-21 06:50:21 +0530

    Developer Skills and Editor Configurations

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+
+// Prettier automatically changes single quotes to double quotes
+const name = "Vignesh";
+
+// Prettier automatically adds the paranthesis fo birthYear- overridden at .prettierrc configuration file
+const returnAge = birthYear => 2021 - birthYear;
+
+console.log(returnAge(1988));
